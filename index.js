f2b44edc8b3664d85a4ed227e985bb89efbc2cc9
parth962359 <146902273+parth962359@users.noreply.github.com>
2025-05-06 22:56:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs=require("fs");
 
-// initialize Express.js server and save as a variable
-// so it can be referred to as `app`
 const app = express();
 
 app.use(bodyParser.json());
-let data =fs.readFileSync("text.json",'utf8') // In-memory storage for todos
+let data =fs.readFileSync("text.json",'utf8') 
 let todos=JSON.parse(data);
 
 console.log(todos);
-// GET endpoint to fetch all todo items
+
 app.get("/todos", (req, res) => {
   res.json(todos);
  
 });
 
-// POST endpoint to create a new todo item
-// provide `title` and optionally `completed` in the request body as JSON
 app.post("/todos", (req, res) => {
   const todo = {
     id: req.body.id,
     title: req.body.title,
-    completed: req.body.completed || false,
+    completed: req.body.completed
   };
 
   todos.push(todo);
@@ -34,8 +30,7 @@ app.post("/todos", (req, res) => {
   res.json("new todo created");
 });
 
-// PUT endpiont to update an existing todo item with the specified `id`
-// provide updated `title` and/or `completed` in the request body as JSON
+
 app.put("/todos/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const todo = todos.find((t) => t.id === id);
@@ -47,7 +42,7 @@ app.put("/todos/:id", (req, res) => {
   res.json(todo);
 });
 
-// DELETE endpoint to remove an existing todo item with the specified `id`
+
 app.delete("/todos/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const index = todos.findIndex((t) => t.id === id);
